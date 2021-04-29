f0381c2a6688808a19d9d504fea757a0c09e7ce4
KeizoAutbank <keizo.kobayashi@linux.autbank>
2021-04-28 22:56:34 -0300

    Estrutura Base do Projeto

---
+++ src/test/java/com/example/olisaude/OliSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
